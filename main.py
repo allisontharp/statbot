6dee514147b6dcd36c474d75092b218117ae5960
Allison <allison.tharp@gmail.com>
2016-11-11 19:07:45 -0500

    Update main.py (added some error handling)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
         
 
 '''
-import praw, obot, os, re
+import praw, obot, os
 from funcs import *
 from operator import itemgetter as ig
 
@@ -27,36 +27,59 @@ else:
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = filter(None, posts_replied_to)
 
-# for submission in subreddit.get_new(limit = 5):
 for comment in all_comments:
     if comment.id not in posts_replied_to:
         body = comment.body
-        # if re.search("/u/stat-bot", comment.body, re.IGNORECASE):
-        if body[0:11] == '/u/stat-bot':
-            mindate = ''
-            maxdate = ''
-            body = body.replace('/u/stat-bot ', '')
-            split = body.split(' ',10)
-            split = [i for i in split if i != ''] #remove any spaces
-            username = split[0]
-            kind = split[1] ## play or collection
+        body = body.split(' ', 10000)
+        body = [i for i in body if i != ''] # remove any spaces
+        if '/u/stat-bot' in body:
+            ind = body.index("/u/stat-bot")
+            # Remove everything before the bot call:
+            for i in xrange(ind+1):
+                body.pop(0)
+            
+            username = body[0]
+            kind = body[1] ## play or collection
+            error = []
             try: 
-                mindate = split[2] ##
+                mindate = body[2] ##
             except IndexError:
-                mindate = ''      
+                mindate = ''
+                
             try:
-                maxdate = split[3]
+                maxdate = body[3]
             except IndexError:
                 maxdate = ''
-
-            if kind == 'plays':     
-                queryplay(username, mindate = mindate)
+            
+            try:
+                validate_date(mindate)
+            except ValueError:
+                error.append('- Start date format incorrect.  Expected YYYY-MM-DD, received "{md}".'.format(md = mindate))
+                
+            try:
+                validate_date(maxdate)
+            except ValueError:
+                error.append('- End date format incorrect.  Expected YYYY-MM-DD, received "{md}".'.format(md = maxdate))    
+            
+            if error == []:
+                if kind == 'plays':     
+                    out = queryplay(username, mindate = mindate, maxdate = maxdate)
+                else:
+                    print "Someone mentioned /u/stat-bot, but didn't call it correctly"
+            else:
+                out = "It looks like we've had one or more errors:\n\n"
+                out += '\n\n'.join(error)
+                out += "\n\nTo call me correctly, use the format:\n\n/u/stat-bot your_username plays startdate enddate \n\nex: /u/stat-bot Octavian plays 2016-1-1 2016-06-30"
+                
             
             
             #print out
             comment.reply(out)
             print "Bot replied to : ", comment.body
             posts_replied_to.append(comment.id)
+        elif '/u/stat-bot' in body:
+            out = ""
+            print '/u/stat-bot was in a comment, but not the first word'
             
 with open("posts_replied_to.txt", "w") as f:
     for post_id in posts_replied_to:
@@ -66,46 +89,3 @@ with open("posts_replied_to.txt", "w") as f:
     
     
 
-# # 
-# # 
-# # 
-# # # from decimal import Decimal as dec
-# # # from funcs import *
-# # # from operator import itemgetter as ig
-# # 
-# # # print "Username: "
-# # # usr = raw_input()
-# usr = 'mad4hatter'
-# mindate = '2016-11-01'
-# maxdate = '2016-11-10'
-# 
-# games = []
-# plays, games = combine_plays(usr, games, mindate=mindate, maxdate=maxdate)
-# #plays, games = combine_plays(usr, games)
-# 
-#  
-# tot_time = sum([int(play.duration) for play in plays])
-# numplays = sum([int(play.quantity) for play in plays])
-# avg_time = tot_time*1.0/numplays
-# totpoints = sum([float(play.totscore) for play in plays]) 
-# [tot, avg] = userscore(usr, plays)
-# 
-# 
-# data = [(game.name, game.quantity, round(game.totduration*1.0/game.quantity,2),
-#          round(game.totscore*1.0/(game.totplayers),2) if game.totplayers != 0 else 0,
-#          round(game.userscore*1.0/game.quantity))        
-#         for game in games]
-# # print ''
-# # print 'Total Plays: ' + str(numplays)
-# sdata = sorted(data, key=ig(1), reverse=True)
-# head = ('Game', 'Total Plays', 'AVG Minutes', 'AVG Score', 'Your AVG')
-# sdata = [head] + sdata
-# # pprint(sdata, lim=10)
-# # 
-# # print '---'
-# # print '---'
-# 
-# tbl = rtable(sdata, lim=5)
-# 
-# print tbl
-# # 
