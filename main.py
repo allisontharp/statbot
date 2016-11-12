b7719e09ba09b0b9ae22801e45ef6be16c383798
Allison <allison.tharp@gmail.com>
2016-11-12 11:27:21 -0500

    Updated error catching
    Fixed error catching for when requester does not specify kind (in this case, only 'play')

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 '''
-# # To Do - Put in database and compare to other users? Could also have a delta function
-            (how many times/hours/games you've played since the last time you asked)
-# # To Do - Save all of the comments people ask         
+# # To Do - Beckon and look at all of the bgg links, give average duration per player for each game
 
 '''
 import praw, obot, os, re
@@ -11,98 +9,101 @@ from operator import itemgetter as ig
 
 user_agent = ('Stat Guy 0.1')
 
-# while True:
-r = obot.login()
-subreddit = r.get_subreddit('statbot')
-all_comments = subreddit.get_comments()
-c, conn = initsql()
-
-
-
-if not os.path.isfile("posts_replied_to.txt"):
-    posts_replied_to = []
-else:
-    with open("posts_replied_to.txt", "r") as f:
-        posts_replied_to = f.read()
-        posts_replied_to = posts_replied_to.split("\n")
-        posts_replied_to = filter(None, posts_replied_to)
-
-for comment in all_comments:
-    if comment.id not in posts_replied_to: # and comment.author.name != 'stat-bot:
-        body = comment.body
-        body = re.sub('["]','',body)
-        body = body.split(' ', 10000)
-        body = [i for i in body if i != ''] # remove any spaces
-        
-        
-        if '/u/stat-bot' in body:
+while True:
+    r = obot.login()
+    subreddit = r.get_subreddit('statbot')
+    all_comments = subreddit.get_comments()
+    c, conn = initsql()
+    
+    
+    
+    if not os.path.isfile("posts_replied_to.txt"):
+        posts_replied_to = []
+    else:
+        with open("posts_replied_to.txt", "r") as f:
+            posts_replied_to = f.read()
+            posts_replied_to = posts_replied_to.split("\n")
+            posts_replied_to = filter(None, posts_replied_to)
+    
+    for comment in all_comments:
+        if comment.id not in posts_replied_to: # and comment.author.name != 'stat-bot:
+            body = comment.body
+            body = re.sub('["]','',body)
+            body = body.split(' ', 10000)
+            body = [i for i in body if i != ''] # remove any spaces
             
-            condensed = ' '.join(body)
-            query = 'INSERT INTO comments (commentid, username, comment) VALUES ("{cid}","{usr}", "{cmt}")'.format(cid = comment.id, usr = comment.author, cmt = condensed)
-            sql(c, query)
             
-            ind = body.index("/u/stat-bot")
-            # Remove everything before the bot call:
-            for i in xrange(ind+1):
-                body.pop(0)
-            
-            username = body[0]
-            if username == 'about':
-                out = '''Hello!  I am a bot that grabs play data for plays logged on board game geek for a specific user.
-\n\nTo call me correctly, use the format:\n\n/u/stat-bot your_username plays startdate enddate \n\nex: /u/stat-bot Octavian plays 2016-1-1 2016-06-30
-\n\nMy code is available [here](https://github.com/allisontharp/statbot).
-\n\nI am always interested in suggestions for improvement.  If you have any suggestions, please message me!'''
-            elif username == 'help':
-                out = 'To call me correctly, use the format:\n\n/u/stat-bot your_username plays startdate enddate \n\nex: /u/stat-bot Octavian plays 2016-1-1 2016-06-30'
-            else:
-                kind = body[1] ## play or collection
-                error = []
-                try: 
-                    mindate = body[2] ##
-                except IndexError:
-                    mindate = ''
-                    
-                try:
-                    maxdate = body[3]
-                except IndexError:
-                    maxdate = ''
+            if '/u/stat-bot' in body:
                 
-                try:
-                    validate_date(mindate)
-                except ValueError:
-                    error.append("- Start date format incorrect.  Expected YYYY-MM-DD, received '{md}'.".format(md = mindate))
-                    
-                try:
-                    validate_date(maxdate)
-                except ValueError:
-                    error.append("- End date format incorrect.  Expected YYYY-MM-DD, received '{md}'.".format(md = maxdate))    
-            
-            
-                if error == []:
-                    if kind == 'plays':     
-                        out = queryplay(c, username, mindate = mindate, maxdate = maxdate)
-                    else:
-                        print "Someone mentioned /u/stat-bot, but didn't call it correctly"
-                        out = "You didn't mention what kind of stats you want, or I am having trouble understanding you.  Right now, I only know plays.  Please use '/u/stat-bot help' if you need help."
+                condensed = ' '.join(body)
+                query = 'INSERT INTO comments (commentid, username, comment) VALUES ("{cid}","{usr}", "{cmt}")'.format(cid = comment.id, usr = comment.author, cmt = condensed)
+                sql(c, query)
+                
+                ind = body.index("/u/stat-bot")
+                # Remove everything before the bot call:
+                for i in xrange(ind+1):
+                    body.pop(0)
+                
+                username = body[0]
+                if username == 'about':
+                    out = '''Hello!  I am a bot that grabs play data for plays logged on board game geek for a specific user.
+    \n\nTo call me correctly, use the format:\n\n/u/stat-bot your_username plays startdate enddate \n\nex: /u/stat-bot Octavian plays 2016-1-1 2016-06-30
+    \n\nMy code is available [here](https://github.com/allisontharp/statbot).
+    \n\nI am always interested in suggestions for improvement.  If you have any suggestions, please message me!'''
+                elif username == 'help':
+                    out = 'To call me correctly, use the format:\n\n/u/stat-bot your_username plays startdate enddate \n\nex: /u/stat-bot Octavian plays 2016-1-1 2016-06-30'
                 else:
-                    out = "It looks like we've had one or more errors:\n\n"
-                    out += '\n\n'.join(error)
-                    out += "\n\nTo call me correctly, use the format:\n\n/u/stat-bot your_username plays startdate enddate \n\nex: /u/stat-bot Octavian plays 2016-1-1 2016-06-30"
+                    error = []
+                    try:
+                        kind = body[1] ## play or collection
+                    except IndexError:
+                        error = []
+                    try: 
+                        mindate = body[2] ##
+                    except IndexError:
+                        mindate = ''
+                        
+                    try:
+                        maxdate = body[3]
+                    except IndexError:
+                        maxdate = ''
+                    
+                    try:
+                        validate_date(mindate)
+                    except ValueError:
+                        error.append("- Start date format incorrect.  Expected YYYY-MM-DD, received '{md}'.".format(md = mindate))
+                        
+                    try:
+                        validate_date(maxdate)
+                    except ValueError:
+                        error.append("- End date format incorrect.  Expected YYYY-MM-DD, received '{md}'.".format(md = maxdate))    
                 
-            
-            
-            #print out
-            comment.reply(out)
-            print "Bot replied to : ", comment.body
-            posts_replied_to.append(comment.id)
+                
+                    if error == []:
+                        if kind == 'plays':     
+                            out = queryplay(c, username, mindate = mindate, maxdate = maxdate)
+                        else:
+                            print "Someone mentioned /u/stat-bot, but didn't call it correctly"
+                            out = "You didn't mention what kind of stats you want, or I am having trouble understanding you.  Right now, I only know plays.  Please use '/u/stat-bot help' if you need help."
+                    else:
+                        out = "It looks like we've had one or more errors:\n\n"
+                        out += '\n\n'.join(error)
+                        out += "\n\nTo call me correctly, use the format:\n\n/u/stat-bot your_username plays startdate enddate \n\nex: /u/stat-bot Octavian plays 2016-1-1 2016-06-30"
+                    
+                
+                
+                #print out
+                comment.reply(out)
+                print "Bot replied to : ", comment.body
+                posts_replied_to.append(comment.id)
+    
+    
+                
+    with open("posts_replied_to.txt", "w") as f:
+        for post_id in posts_replied_to:
+            f.write(post_id + "\n")
 
-
-            
-with open("posts_replied_to.txt", "w") as f:
-    for post_id in posts_replied_to:
-        f.write(post_id + "\n")
-
-    # time.sleep(1800)
+    time.sleep(900)
     
     
 
