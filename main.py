7b6c55890d6528da98bbdf1927087f6f9c2fc98d
Allison <allison.tharp@gmail.com>
2016-11-12 23:00:25 -0500

    Implemented SQLite DB
    Moved all of the data to SQLite DB.  This will make future querying easier and will allow for better statistics.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 '''
 # # To Do - Beckon and look at all of the bgg links, give average duration per player for each game
-
+# # To Do - Ignore play duration where duration is 0.
+# # To Do - Incorporate play quantity
 '''
-import praw, obot, os, re
-from funcs import *
+import praw, obot, re
+from funcs_sql import *
 from operator import itemgetter as ig
 
 
@@ -15,27 +16,25 @@ while True:
     all_comments = subreddit.get_comments()
     c, conn = initsql()
     
+    comm_replied_to = []
     
-    
-    if not os.path.isfile("posts_replied_to.txt"):
-        posts_replied_to = []
-    else:
-        with open("posts_replied_to.txt", "r") as f:
-            posts_replied_to = f.read()
-            posts_replied_to = posts_replied_to.split("\n")
-            posts_replied_to = filter(None, posts_replied_to)
+    query = "SELECT commentid FROM comments"
+    out = sql(c, query)
+    comm_replied_to = [str(i[0]) for i in out]
     
     for comment in all_comments:
-        if comment.id not in posts_replied_to: # and comment.author.name != 'stat-bot:
+        if comment.id not in comm_replied_to: # and comment.author.name != 'stat-bot:
             body = comment.body
+            body = body.replace('\n', ' ')
             body = re.sub('["]','',body)
             body = body.split(' ', 10000)
             body = [i for i in body if i != ''] # remove any spaces
             
-            
             if '/u/stat-bot' in body:
-                
+                print body
+
                 condensed = ' '.join(body)
+
                 query = 'INSERT INTO comments (commentid, username, comment) VALUES ("{cid}","{usr}", "{cmt}")'.format(cid = comment.id, usr = comment.author, cmt = condensed)
                 sql(c, query)
                 
@@ -70,39 +69,39 @@ while True:
                         maxdate = ''
                     
                     try:
-                        validate_date(mindate)
+                        mindate = validate_date(mindate)
                     except ValueError:
                         error.append("- Start date format incorrect.  Expected YYYY-MM-DD, received '{md}'.".format(md = mindate))
                         
                     try:
-                        validate_date(maxdate)
+                        maxdate = validate_date(maxdate)
                     except ValueError:
                         error.append("- End date format incorrect.  Expected YYYY-MM-DD, received '{md}'.".format(md = maxdate))    
                 
                 
                     if error == []:
+                        isfail = 0
                         if kind == 'plays':     
-                            out = queryplay(c, username, mindate = mindate, maxdate = maxdate)
+                            out = playsmain(c, username, mindate = mindate, maxdate = maxdate)
                         else:
                             print "Someone mentioned /u/stat-bot, but didn't call it correctly"
                             out = "You didn't mention what kind of stats you want, or I am having trouble understanding you.  Right now, I only know plays.  Please use '/u/stat-bot help' if you need help."
                     else:
+                        isfail = 1
                         out = "It looks like we've had one or more errors:\n\n"
                         out += '\n\n'.join(error)
-                        out += "\n\nTo call me correctly, use the format:\n\n/u/stat-bot your_username plays startdate enddate \n\nex: /u/stat-bot Octavian plays 2016-1-1 2016-06-30"
+                        out += "\n\nTo call me correctly, use the format:\n\n/u/stat-bot your_username plays startdate enddate \n\nex: /u/stat-bot Octavian plays 2016-01-01 2016-06-30"
                     
                 
-                
+                print out
                 comment.reply(out)
                 print "Bot replied to : ", comment.body
-                posts_replied_to.append(comment.id)
-                conn.commit()
-  
+                query = '''INSERT INTO comments (commentid, username, comment, isfail, bggusername) VALUES
+("{cid}", "{usr}", "{cmt}", {isfail}, "{bgg}")'''.format(cid = comment.id, usr = comment.author, cmt = comment.body, isfail = isfail, bgg = username)
                 
-                with open("posts_replied_to.txt", "w") as f:
-                    for post_id in posts_replied_to:
-                        f.write(post_id + "\n")
-
+                conn.commit()
+                time.sleep(10)
+    
     time.sleep(900)
     
     
